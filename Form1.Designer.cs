b71375c3e82040d1cefcb61b71e8aa17389c3ca9
Fernando Gonzalez <gonzalez17f@icloud.com>
2022-02-15 22:16:02 -0600

    Added a bit of styling. Reordered groups. Added two buttons. This is to follow the software requirements on the assignment.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,248 +28,135 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.MenuPanel = new System.Windows.Forms.Panel();
-            this.LogoPanel = new System.Windows.Forms.Panel();
-            this.NameGroupBox = new System.Windows.Forms.GroupBox();
-            this.LastNameTextBox = new System.Windows.Forms.TextBox();
-            this.FirstNameTextBox = new System.Windows.Forms.TextBox();
+            this.BackgroundPanel = new System.Windows.Forms.Panel();
+            this.ResultsButton = new FontAwesome.Sharp.IconButton();
+            this.BMIChartButton = new FontAwesome.Sharp.IconButton();
+            this.HeightWeightGroupBox = new System.Windows.Forms.GroupBox();
+            this.DOBGroupBox = new System.Windows.Forms.GroupBox();
             this.GenderGroupBox = new System.Windows.Forms.GroupBox();
-            this.FemaleRadioButton = new System.Windows.Forms.RadioButton();
-            this.MaleRadioButton = new System.Windows.Forms.RadioButton();
-            this.DobGroupBox = new System.Windows.Forms.GroupBox();
-            this.DobBox = new System.Windows.Forms.DateTimePicker();
-            this.HeightWeightBox = new System.Windows.Forms.GroupBox();
-            this.WeightTextBox = new System.Windows.Forms.TextBox();
-            this.WeightTrackBar = new System.Windows.Forms.TrackBar();
-            this.HeightComboBox = new System.Windows.Forms.ComboBox();
-            this.ResultPanel = new System.Windows.Forms.Panel();
-            this.MenuPanel.SuspendLayout();
-            this.NameGroupBox.SuspendLayout();
-            this.GenderGroupBox.SuspendLayout();
-            this.DobGroupBox.SuspendLayout();
-            this.HeightWeightBox.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.WeightTrackBar)).BeginInit();
+            this.FullNameGroupBox = new System.Windows.Forms.GroupBox();
+            this.BackgroundPanel.SuspendLayout();
             this.SuspendLayout();
             // 
-            // MenuPanel
+            // BackgroundPanel
             // 
-            this.MenuPanel.Controls.Add(this.LogoPanel);
-            this.MenuPanel.Dock = System.Windows.Forms.DockStyle.Left;
-            this.MenuPanel.Location = new System.Drawing.Point(0, 0);
-            this.MenuPanel.Name = "MenuPanel";
-            this.MenuPanel.Size = new System.Drawing.Size(263, 834);
-            this.MenuPanel.TabIndex = 0;
+            this.BackgroundPanel.BackColor = System.Drawing.Color.LightCoral;
+            this.BackgroundPanel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.BackgroundPanel.Controls.Add(this.ResultsButton);
+            this.BackgroundPanel.Controls.Add(this.BMIChartButton);
+            this.BackgroundPanel.Controls.Add(this.HeightWeightGroupBox);
+            this.BackgroundPanel.Controls.Add(this.DOBGroupBox);
+            this.BackgroundPanel.Controls.Add(this.GenderGroupBox);
+            this.BackgroundPanel.Controls.Add(this.FullNameGroupBox);
+            this.BackgroundPanel.Location = new System.Drawing.Point(0, 0);
+            this.BackgroundPanel.Name = "BackgroundPanel";
+            this.BackgroundPanel.Size = new System.Drawing.Size(1192, 834);
+            this.BackgroundPanel.TabIndex = 0;
             // 
-            // LogoPanel
+            // ResultsButton
             // 
-            this.LogoPanel.Dock = System.Windows.Forms.DockStyle.Top;
-            this.LogoPanel.Location = new System.Drawing.Point(0, 0);
-            this.LogoPanel.Name = "LogoPanel";
-            this.LogoPanel.Size = new System.Drawing.Size(263, 200);
-            this.LogoPanel.TabIndex = 0;
+            this.ResultsButton.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ResultsButton.FlatAppearance.BorderSize = 0;
+            this.ResultsButton.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ResultsButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ResultsButton.Font = new System.Drawing.Font("Franklin Gothic Medium", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ResultsButton.IconChar = FontAwesome.Sharp.IconChar.ChartLine;
+            this.ResultsButton.IconColor = System.Drawing.Color.DeepSkyBlue;
+            this.ResultsButton.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.ResultsButton.Location = new System.Drawing.Point(607, 376);
+            this.ResultsButton.Name = "ResultsButton";
+            this.ResultsButton.Size = new System.Drawing.Size(284, 91);
+            this.ResultsButton.TabIndex = 5;
+            this.ResultsButton.Text = "Results";
+            this.ResultsButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.ResultsButton.UseVisualStyleBackColor = false;
             // 
-            // NameGroupBox
+            // BMIChartButton
             // 
-            this.NameGroupBox.Controls.Add(this.LastNameTextBox);
-            this.NameGroupBox.Controls.Add(this.FirstNameTextBox);
-            this.NameGroupBox.Location = new System.Drawing.Point(269, 0);
-            this.NameGroupBox.Name = "NameGroupBox";
-            this.NameGroupBox.Size = new System.Drawing.Size(504, 160);
-            this.NameGroupBox.TabIndex = 1;
-            this.NameGroupBox.TabStop = false;
-            this.NameGroupBox.Text = "Name";
+            this.BMIChartButton.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.BMIChartButton.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.BMIChartButton.FlatAppearance.BorderSize = 0;
+            this.BMIChartButton.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.HotTrack;
+            this.BMIChartButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BMIChartButton.Font = new System.Drawing.Font("Franklin Gothic Medium", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.BMIChartButton.IconChar = FontAwesome.Sharp.IconChar.ChartPie;
+            this.BMIChartButton.IconColor = System.Drawing.Color.DeepSkyBlue;
+            this.BMIChartButton.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.BMIChartButton.Location = new System.Drawing.Point(317, 376);
+            this.BMIChartButton.Name = "BMIChartButton";
+            this.BMIChartButton.Size = new System.Drawing.Size(284, 91);
+            this.BMIChartButton.TabIndex = 4;
+            this.BMIChartButton.Text = "BMI Chart";
+            this.BMIChartButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.BMIChartButton.UseVisualStyleBackColor = false;
             // 
-            // LastNameTextBox
+            // HeightWeightGroupBox
             // 
-            this.LastNameTextBox.ForeColor = System.Drawing.Color.Gainsboro;
-            this.LastNameTextBox.Location = new System.Drawing.Point(6, 80);
-            this.LastNameTextBox.Name = "LastNameTextBox";
-            this.LastNameTextBox.Size = new System.Drawing.Size(491, 39);
-            this.LastNameTextBox.TabIndex = 1;
-            this.LastNameTextBox.Text = "Last Name";
+            this.HeightWeightGroupBox.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.HeightWeightGroupBox.Font = new System.Drawing.Font("Franklin Gothic Medium", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.HeightWeightGroupBox.Location = new System.Drawing.Point(607, 174);
+            this.HeightWeightGroupBox.Name = "HeightWeightGroupBox";
+            this.HeightWeightGroupBox.Size = new System.Drawing.Size(569, 158);
+            this.HeightWeightGroupBox.TabIndex = 3;
+            this.HeightWeightGroupBox.TabStop = false;
+            this.HeightWeightGroupBox.Text = "Height (in) / Weight (lb)";
             // 
-            // FirstNameTextBox
+            // DOBGroupBox
             // 
-            this.FirstNameTextBox.ForeColor = System.Drawing.Color.Gainsboro;
-            this.FirstNameTextBox.Location = new System.Drawing.Point(6, 38);
-            this.FirstNameTextBox.Name = "FirstNameTextBox";
-            this.FirstNameTextBox.Size = new System.Drawing.Size(491, 39);
-            this.FirstNameTextBox.TabIndex = 0;
-            this.FirstNameTextBox.Text = "First Name";
+            this.DOBGroupBox.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.DOBGroupBox.Font = new System.Drawing.Font("Franklin Gothic Medium", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.DOBGroupBox.Location = new System.Drawing.Point(10, 174);
+            this.DOBGroupBox.Name = "DOBGroupBox";
+            this.DOBGroupBox.Size = new System.Drawing.Size(591, 158);
+            this.DOBGroupBox.TabIndex = 2;
+            this.DOBGroupBox.TabStop = false;
+            this.DOBGroupBox.Text = "Date of Birth";
             // 
             // GenderGroupBox
             // 
-            this.GenderGroupBox.Controls.Add(this.FemaleRadioButton);
-            this.GenderGroupBox.Controls.Add(this.MaleRadioButton);
-            this.GenderGroupBox.Location = new System.Drawing.Point(269, 162);
+            this.GenderGroupBox.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.GenderGroupBox.Font = new System.Drawing.Font("Franklin Gothic Medium", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.GenderGroupBox.Location = new System.Drawing.Point(607, 10);
             this.GenderGroupBox.Name = "GenderGroupBox";
-            this.GenderGroupBox.Size = new System.Drawing.Size(503, 156);
-            this.GenderGroupBox.TabIndex = 2;
+            this.GenderGroupBox.Size = new System.Drawing.Size(569, 158);
+            this.GenderGroupBox.TabIndex = 1;
             this.GenderGroupBox.TabStop = false;
             this.GenderGroupBox.Text = "Gender";
             // 
-            // FemaleRadioButton
+            // FullNameGroupBox
             // 
-            this.FemaleRadioButton.AutoSize = true;
-            this.FemaleRadioButton.Location = new System.Drawing.Point(6, 80);
-            this.FemaleRadioButton.Name = "FemaleRadioButton";
-            this.FemaleRadioButton.Size = new System.Drawing.Size(122, 36);
-            this.FemaleRadioButton.TabIndex = 1;
-            this.FemaleRadioButton.TabStop = true;
-            this.FemaleRadioButton.Text = "Female";
-            this.FemaleRadioButton.UseVisualStyleBackColor = true;
-            // 
-            // MaleRadioButton
-            // 
-            this.MaleRadioButton.AutoSize = true;
-            this.MaleRadioButton.Location = new System.Drawing.Point(6, 38);
-            this.MaleRadioButton.Name = "MaleRadioButton";
-            this.MaleRadioButton.Size = new System.Drawing.Size(98, 36);
-            this.MaleRadioButton.TabIndex = 0;
-            this.MaleRadioButton.TabStop = true;
-            this.MaleRadioButton.Text = "Male";
-            this.MaleRadioButton.UseVisualStyleBackColor = true;
-            // 
-            // DobGroupBox
-            // 
-            this.DobGroupBox.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.DobGroupBox.Controls.Add(this.DobBox);
-            this.DobGroupBox.Location = new System.Drawing.Point(269, 324);
-            this.DobGroupBox.Name = "DobGroupBox";
-            this.DobGroupBox.Size = new System.Drawing.Size(503, 96);
-            this.DobGroupBox.TabIndex = 3;
-            this.DobGroupBox.TabStop = false;
-            this.DobGroupBox.Text = "Date of Birth";
-            // 
-            // DobBox
-            // 
-            this.DobBox.CalendarForeColor = System.Drawing.Color.Gainsboro;
-            this.DobBox.CalendarTitleForeColor = System.Drawing.Color.Gainsboro;
-            this.DobBox.Location = new System.Drawing.Point(6, 38);
-            this.DobBox.MaxDate = new System.DateTime(2022, 2, 15, 0, 0, 0, 0);
-            this.DobBox.MinDate = new System.DateTime(1900, 1, 1, 0, 0, 0, 0);
-            this.DobBox.Name = "DobBox";
-            this.DobBox.Size = new System.Drawing.Size(485, 39);
-            this.DobBox.TabIndex = 0;
-            this.DobBox.Value = new System.DateTime(2022, 2, 15, 0, 0, 0, 0);
-            // 
-            // HeightWeightBox
-            // 
-            this.HeightWeightBox.Controls.Add(this.WeightTextBox);
-            this.HeightWeightBox.Controls.Add(this.WeightTrackBar);
-            this.HeightWeightBox.Controls.Add(this.HeightComboBox);
-            this.HeightWeightBox.Location = new System.Drawing.Point(269, 426);
-            this.HeightWeightBox.Name = "HeightWeightBox";
-            this.HeightWeightBox.Size = new System.Drawing.Size(491, 172);
-            this.HeightWeightBox.TabIndex = 4;
-            this.HeightWeightBox.TabStop = false;
-            this.HeightWeightBox.Text = "Height (in) / Weight (lb)";
-            this.HeightWeightBox.Enter += new System.EventHandler(this.HeightGroupBox_Enter);
-            // 
-            // WeightTextBox
-            // 
-            this.WeightTextBox.Location = new System.Drawing.Point(320, 84);
-            this.WeightTextBox.Name = "WeightTextBox";
-            this.WeightTextBox.ReadOnly = true;
-            this.WeightTextBox.Size = new System.Drawing.Size(165, 39);
-            this.WeightTextBox.TabIndex = 7;
-            // 
-            // WeightTrackBar
-            // 
-            this.WeightTrackBar.Location = new System.Drawing.Point(6, 84);
-            this.WeightTrackBar.Maximum = 400;
-            this.WeightTrackBar.Name = "WeightTrackBar";
-            this.WeightTrackBar.Size = new System.Drawing.Size(308, 90);
-            this.WeightTrackBar.TabIndex = 6;
-            this.WeightTrackBar.TickStyle = System.Windows.Forms.TickStyle.None;
-            this.WeightTrackBar.Scroll += new System.EventHandler(this.trackBar1_Scroll);
-            // 
-            // HeightComboBox
-            // 
-            this.HeightComboBox.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.HeightComboBox.FormattingEnabled = true;
-            this.HeightComboBox.Items.AddRange(new object[] {
-            "4\'8\"",
-            "4\'9\"",
-            "4\'10\"",
-            "4\'11\"",
-            "5\'0\"",
-            "5\'1\"",
-            "5\'2\"",
-            "5\'3\"",
-            "5\'4\"",
-            "5\'5\"",
-            "5\'6\"",
-            "5\'7\"",
-            "5\'8\"",
-            "5\'9\"",
-            "5\'10\"",
-            "5\'11\"",
-            "6\'0\"",
-            "6\'1\"",
-            "6\'2\"",
-            "6\'3\"",
-            "6\'4\"",
-            "6\'5\"",
-            "6\'6\"",
-            "6\'7\"",
-            "6\'8\""});
-            this.HeightComboBox.Location = new System.Drawing.Point(6, 38);
-            this.HeightComboBox.Name = "HeightComboBox";
-            this.HeightComboBox.Size = new System.Drawing.Size(485, 40);
-            this.HeightComboBox.TabIndex = 5;
-            // 
-            // ResultPanel
-            // 
-            this.ResultPanel.Dock = System.Windows.Forms.DockStyle.Right;
-            this.ResultPanel.Location = new System.Drawing.Point(790, 0);
-            this.ResultPanel.Name = "ResultPanel";
-            this.ResultPanel.Size = new System.Drawing.Size(400, 834);
-            this.ResultPanel.TabIndex = 5;
+            this.FullNameGroupBox.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.FullNameGroupBox.Font = new System.Drawing.Font("Franklin Gothic Medium", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.FullNameGroupBox.Location = new System.Drawing.Point(10, 10);
+            this.FullNameGroupBox.Name = "FullNameGroupBox";
+            this.FullNameGroupBox.Size = new System.Drawing.Size(591, 158);
+            this.FullNameGroupBox.TabIndex = 0;
+            this.FullNameGroupBox.TabStop = false;
+            this.FullNameGroupBox.Text = "Full Name";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1190, 834);
-            this.Controls.Add(this.ResultPanel);
-            this.Controls.Add(this.HeightWeightBox);
-            this.Controls.Add(this.DobGroupBox);
-            this.Controls.Add(this.GenderGroupBox);
-            this.Controls.Add(this.NameGroupBox);
-            this.Controls.Add(this.MenuPanel);
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1190, 554);
+            this.Controls.Add(this.BackgroundPanel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
             this.Text = "Form1";
-            this.MenuPanel.ResumeLayout(false);
-            this.NameGroupBox.ResumeLayout(false);
-            this.NameGroupBox.PerformLayout();
-            this.GenderGroupBox.ResumeLayout(false);
-            this.GenderGroupBox.PerformLayout();
-            this.DobGroupBox.ResumeLayout(false);
-            this.HeightWeightBox.ResumeLayout(false);
-            this.HeightWeightBox.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.WeightTrackBar)).EndInit();
+            this.BackgroundPanel.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private Panel MenuPanel;
-        private Panel LogoPanel;
-        private GroupBox NameGroupBox;
+        private Panel BackgroundPanel;
+        private GroupBox HeightWeightGroupBox;
+        private GroupBox DOBGroupBox;
         private GroupBox GenderGroupBox;
-        private TextBox LastNameTextBox;
-        private TextBox FirstNameTextBox;
-        private RadioButton FemaleRadioButton;
-        private RadioButton MaleRadioButton;
-        private GroupBox DobGroupBox;
-        private DateTimePicker DobBox;
-        private GroupBox HeightWeightBox;
-        private ComboBox HeightComboBox;
-        private TrackBar WeightTrackBar;
-        private TextBox WeightTextBox;
-        private Panel ResultPanel;
+        private GroupBox FullNameGroupBox;
+        private FontAwesome.Sharp.IconButton ResultsButton;
+        private FontAwesome.Sharp.IconButton BMIChartButton;
     }
 }
